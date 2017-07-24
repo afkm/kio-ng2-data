374b19ae0f3f663d790ed31a4c2da4c992c70ded
Matthias Buchholz <matthias@afkm.io>
2017-07-24 17:02:39 +0200

    optional modifiers key

--- release/components/interfaces/component.js
+++ release/components/interfaces/component.js
@@ -1,7 +1,7 @@
+"use strict";
 /**
  * @module kio-ng2-component-routing
  * @namespace Components
  */
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 //# sourceMappingURL=component.js.map
